0354d9bb2ff3dfd77f884d1f0170d3d957831c15
huzhifeng <huzhifeng@douyu.tv>
2017-04-05 14:15:38 +0800

    Add async get data

--- langfight-uwp/MainPage.xaml.cs
+++ langfight-uwp/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -12,6 +13,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.Web.Http;
 
 // https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
 
@@ -25,6 +27,22 @@ namespace langfight_uwp
         public MainPage()
         {
             this.InitializeComponent();
+            ListView lv = new ListView();
+            lv.Items.Add("Item a");
+            lv.Items.Add("Item b");
+            spMain.Children.Add(lv);
+            Debug.WriteLine("nothing possible");
+            GetData();
         }
+        public async void GetData()
+        {
+            var uri = new Uri("https://www.netroby.com");
+
+            var client = new HttpClient();
+            var resp = await client.GetAsync(uri);
+            var result = await resp.Content.ReadAsStringAsync();
+            Debug.WriteLine(result);
+        }
+        
     }
 }
